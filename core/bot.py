f3c2cea1d50d1f87eb9d45f4185a4a85f5290947
loilv <loilv2@vetc.com.vn>
2025-11-12 19:24:47 +0700

    u

--- core/bot.py
+++ core/bot.py
@@ -251,25 +251,25 @@ class CandlePatternScannerBot:
             )
             return
 
-        if 70 < abs(precent_sell) <= 75 or 70 < abs(precent_buy) <= 75:
-            side = "SELL" if percentage_change > 0 else "BUY"
-            if not self.can_order(symbol, side):
-                return
-
-            adjust = 0.9995 if side == "BUY" else 1.0005
-            entry_price = close_price * adjust
-
-            qty = self.order_manager.calculate_position_size(symbol, entry_price)
-
-            logging.info(f"[ENTRY] Ngược chiều: {side} {symbol} | Qty: {qty} | Price: {entry_price:.5f}")
-
-            self.position[symbol] = {}
-            self.trailing_stop[symbol] = {"counter": True}
-
-            self.binance_watcher.create_entry_order(
-                symbol, side, round(entry_price, 5), qty
-            )
-            return
+        # if 70 < abs(precent_sell) <= 75 or 70 < abs(precent_buy) <= 75:
+        #     side = "SELL" if percentage_change > 0 else "BUY"
+        #     if not self.can_order(symbol, side):
+        #         return
+        #
+        #     adjust = 0.9995 if side == "BUY" else 1.0005
+        #     entry_price = close_price * adjust
+        #
+        #     qty = self.order_manager.calculate_position_size(symbol, entry_price)
+        #
+        #     logging.info(f"[ENTRY] Ngược chiều: {side} {symbol} | Qty: {qty} | Price: {entry_price:.5f}")
+        #
+        #     self.position[symbol] = {}
+        #     self.trailing_stop[symbol] = {"counter": True}
+        #
+        #     self.binance_watcher.create_entry_order(
+        #         symbol, side, round(entry_price, 5), qty
+        #     )
+        #     return
 
         # t_open = kline['t'] / 1000
         # now = time.time()
