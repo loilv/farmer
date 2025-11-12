1018d58749b068acbefc901d948d12446f3980e4
loilv <loilv2@vetc.com.vn>
2025-11-12 23:27:19 +0700

    u

--- core/bot.py
+++ core/bot.py
@@ -264,7 +264,7 @@ class CandlePatternScannerBot:
 
         if candle_duration > 840:
             return
-        if abs(percentage_change) > 5:
+        if abs(percentage_change) > 3:
             print(
             f'Check tín hiệu {symbol} | open: {open_price} | close: {close_price} | h: {h_price} | l: {l_price} | body: {percentage_change}% | ratio_sell: {precent_sell}% | ratio_buy: {precent_buy}%')
         if abs(percentage_change) > 5:
@@ -288,7 +288,7 @@ class CandlePatternScannerBot:
                 )
                 return
         if abs(percentage_change) >= 5:
-            if 75 < abs(precent_sell) <= 80 or 75 < abs(precent_buy) <= 80:
+            if 70 < abs(precent_sell) <= 75 or 70 < abs(precent_buy) <= 75:
                 side = "SELL" if percentage_change > 0 else "BUY"
                 if not self.can_order(symbol, side):
                     return
