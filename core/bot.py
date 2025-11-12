81a7eddf1a3f0616e8c56b4456db932a09a9cca4
loilv <loilv2@vetc.com.vn>
2025-11-12 21:54:24 +0700

    u

--- core/bot.py
+++ core/bot.py
@@ -231,27 +231,27 @@ class CandlePatternScannerBot:
         print(
             f'Check tín hiệu {symbol} | open: {open_price} | close: {close_price} | h: {h_price} | l: {l_price} | body: {percentage_change}% | ratio_sell: {precent_sell}% | ratio_buy: {precent_buy}%')
 
-        # if 99 < abs(precent_sell) <= 100 or 99 < abs(precent_buy) <= 100:
-        #     side = "BUY" if percentage_change > 0 else "SELL"
-        #     if not self.can_order(symbol, side):
-        #         return
-        #
-        #     adjust = 1.0005 if side == "BUY" else 0.9995
-        #     entry_price = close_price * adjust
-        #
-        #     qty = self.order_manager.calculate_position_size(symbol, entry_price)
-        #
-        #     logging.info(f"[ENTRY] Cùng chiều: {side} {symbol} | Qty: {qty} | Price: {entry_price:.5f}")
-        #
-        #     self.position[symbol] = {}
-        #     self.trailing_stop[symbol] = {"counter": True}
-        #
-        #     self.binance_watcher.create_entry_order(
-        #         symbol, side, round(entry_price, 5), qty
-        #     )
-        #     return
+        if 99 < abs(precent_sell) <= 100 or 99 < abs(precent_buy) <= 100:
+            side = "BUY" if percentage_change > 0 else "SELL"
+            if not self.can_order(symbol, side):
+                return
 
-        if 75 < abs(precent_sell) <= 80 or 75 < abs(precent_buy) <= 80:
+            adjust = 1.0005 if side == "BUY" else 0.9995
+            entry_price = close_price * adjust
+
+            qty = self.order_manager.calculate_position_size(symbol, entry_price)
+
+            logging.info(f"[ENTRY] Cùng chiều: {side} {symbol} | Qty: {qty} | Price: {entry_price:.5f}")
+
+            self.position[symbol] = {}
+            self.trailing_stop[symbol] = {"counter": True}
+
+            self.binance_watcher.create_entry_order(
+                symbol, side, round(entry_price, 5), qty
+            )
+            return
+
+        if 80 < abs(precent_sell) <= 90 or 80 < abs(precent_buy) <= 90:
             side = "SELL" if percentage_change > 0 else "BUY"
             if not self.can_order(symbol, side):
                 return
