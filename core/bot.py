552b35dcee1afb49a1c6605be400e138377ceb56
loilv <loilv2@vetc.com.vn>
2025-11-13 14:54:16 +0700

    u

--- core/bot.py
+++ core/bot.py
@@ -263,9 +263,9 @@ class CandlePatternScannerBot:
 
             return
 
-        if abs(percentage_change) >= 3:
-            print(
-            f'Check tín hiệu {symbol} | open: {open_price} | close: {close_price} | h: {h_price} | l: {l_price} | body: {percentage_change}% | ratio_sell: {precent_sell}% | ratio_buy: {precent_buy}%')
+        # if abs(percentage_change) >= 3:
+        #     print(
+        #     f'Check tín hiệu {symbol} | open: {open_price} | close: {close_price} | h: {h_price} | l: {l_price} | body: {percentage_change}% | ratio_sell: {precent_sell}% | ratio_buy: {precent_buy}%')
 
         if abs(percentage_change) >= 5 and 180 < candle_duration > 240:
             if 80 < abs(precent_sell) <= 85 or 80 < abs(precent_buy) <= 85:
